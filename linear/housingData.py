bde13480ebc6f8f921013b7da0a294b524f59726
Logan Wingard <wingarlo@oregonstate.edu>
2018-04-09 13:38:22 -0700

    1.1 done

--- linear/housingData.py
+++ linear/housingData.py
@@ -1,3 +1,4 @@
+import numpy as np
 temp = []
 x = []
 y = []
@@ -5,8 +6,32 @@ with open('./housing_train.txt') as f:
 	lines = f.read().split()
 for count in range (len(lines)):
 	if (count+1) % 14 == 0:
-		y.append(lines[count])
+		y.append([float(lines[count])])
 	else:
-		temp.append(lines[count])
-x = [temp[i:i+13] for i in range(0,len(temp),13)]
-print x
+		if (count+1) % 14 == 1:
+			temp.append(1.)
+		temp.append(float(lines[count]))
+x = [temp[i:i+14] for i in range(0,len(temp),14)]
+x1 = np.matrix(x)
+y1 = np.matrix(y)
+w = np.matrix([])
+w = (x1.T * x1).I * x1.T * y1
+#print w
+
+
+with open('./housing_test.txt') as f:
+	lines = f.read().split()
+for count in range (0,len(lines)):
+	if (count+1) % 14 == 0:
+		y.append([float(lines[count])])
+	else:
+		if (count+1) % 14 == 1:
+			temp.append(1.)
+		temp.append(float(lines[count]))
+x = [temp[i:i+14] for i in range(0,len(temp),14)]
+x1 = np.matrix(x)
+y1 = np.matrix(y)
+error = []
+for i in range (len(y1)):
+	error.append(y1[i] - x1[i]*w)
+print error
