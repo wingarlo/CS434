9e5ce2bfc5aac2e4edee016c02522617020c368a
Logan Wingard <wingarlo@oregonstate.edu>
2018-04-27 16:01:03 -0700

    spaghetti

--- as2/DT/dt.py
+++ as2/DT/dt.py
@@ -2,6 +2,9 @@ import numpy as np
 import math
 import sys
 import csv
+gains = []
+thresholds = []
+tree = []
 def sortByFeature(x,y,j):
 	for i in range(1, len(y)):
 		k = i-1 
@@ -14,7 +17,7 @@ def sortByFeature(x,y,j):
 		x[k+1,j] = keyx
 		y[k+1,0] = keyy
 	return x,y
-	
+
 def entrop(occur, tot):
 	occur = float(occur)
 	tot = float(tot)
@@ -93,13 +96,91 @@ def DT():
 			bad+= 1
 	print "accuracy on training data: "
 	print float(good/float(good+bad))
+	
+def recurDT(x,y,d):
+	maxGain = [0,0,0]
+	if d!=0:
+		parent = np.count_nonzero(y == 1)				#number of 1's in y
+		pEntrop = entrop(parent, len(y))
+		for j in range(0,30):
+			tempx,tempy = sortByFeature(x,y,j)
+			for i in range(1,len(y)):
+				if(tempy[i] == tempy[i-1]):						#only need to compute information gain when class label changes
+					continue
+				ch1 = np.count_nonzero(tempy[:len(tempy)-i] == 1)
+				ch2 = np.count_nonzero(tempy[i:] == 1)
+				ch1Entrop = entrop(ch1,len(tempy[:len(tempy)-i]))
+				ch2Entrop = entrop(ch2,len(tempy[i:]))
+				gain = float(pEntrop - float((float(len(tempy[:len(tempy)-i]))/len(y)))*ch1Entrop) + (float(float(len(tempy[i:]))/len(y))*ch2Entrop)
+				if gain > maxGain[0]:
+					maxGain = [gain,i,j]
+		tempx,tempy = sortByFeature(x,y,maxGain[2])
+		
+		thresholds.append(float(tempx[maxGain[1], maxGain[2]]))
+		gains.append(maxGain[2])
+		newx = x
+		x = x[x[:,maxGain[2]].argsort()]
+		
+		for i in range(0,len(x)):
+			for j in range(0,len(x[i,0])):
+				newx[i,j] = x[i,0,j]
+		x = newx
+		x = x[:len(x)-maxGain[1]]
+		y = y[:len(y)-maxGain[1]]
+		dummy, y = sortByFeature(x,y,maxGain[2])
+		tree.append(recurDT(x,y,d-1))
+	
+	
+
+	return x
+	
+def DeepTree(d):
+	x,y = loadCSV("knn_train.csv")
+	tree.append(recurDT(x,y,d))
+	x,y = loadCSV("knn_test.csv")
+	good = 0
+	bad = 0
+	for q in range(0,d):
+		newx = x
+		x = x[x[:,gains[q]].argsort()]
+		
+		for i in range(0,len(x)):
+			for j in range(0,len(x[i,0])):
+				newx[i,j] = x[i,0,j]
+		x = newx
+	
+
+		for i in range(0,len(y)):
+			if (float(x[i,gains[q]]) >= float(thresholds[q])) and (float(y[i]) == 1): 
+				good+= 1
+			elif (float(x[i,gains[q]]) <= float(thresholds[q])) and (float(y[i]) != 1): 
+				good+= 1
+			else:
+				bad+= 1
+	print "accuracy on testing data: "
+	print float(good/float(good+bad))
+
+	x,y = loadCSV("knn_train.csv")
+	good = 0
+	bad = 0
+	for q in range(0,d):
+		newx = x
+		x = x[x[:,gains[q]].argsort()]
+		
+		for i in range(0,len(x)):
+			for j in range(0,len(x[i,0])):
+				newx[i,j] = x[i,0,j]
+		x = newx
+	
+
+		for i in range(0,len(y)):
+			if (float(x[i,gains[q]]) >= float(thresholds[q])) and (float(y[i]) == 1): 
+				good+= 1
+			elif (float(x[i,gains[q]]) <= float(thresholds[q])) and (float(y[i]) != 1): 
+				good+= 1
+			else:
+				bad+= 1
+	print "accuracy on training data: "
+	print float(good/float(good+bad))
 DT()
-'''
-print "PRE SORTED:"
-for i in range(0,len(y)):
-	print x[i,0], y[i,0]
-tempx,tempy = sortByFeature(x,y,0)
-print "SORTED"
-for i in range(0,len(y)):
-	print tempx[i,0], tempy[i,0]
-	'''
+DeepTree(12)
