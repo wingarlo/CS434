a6d9dc00a74ec2ef4b616717c32d24f9bb8c4268
Daniel Ross <rossda@vm-cs434-1.engr.oregonstate.edu>
2018-06-05 16:17:50 -0700

    timeParse added

--- mini-proj/loadingdata.py
+++ mini-proj/loadingdata.py
@@ -18,7 +18,9 @@ def loadCSV(dataFile,indicesFile):
 		for j in range(len(dataset[0])):
 			if j != 0:
 				castData[i].append(float(dataset[i][j]))
-		castData[i][0] = int(indices[i][0])
+			else:
+				castData[i].append(timeParse(dataset[i][j]))
+		castData[i].insert(0,int(indices[i][0]))
 	for x in range(len(castData)):
 		keys.append(castData[x][-1])
 		features.append(castData[x][:-1])
@@ -30,22 +32,27 @@ def compileHalfHour(keys,features,castData):
 	lasttime = int(castData[-1][0])
 	halfHours = (lasttime//7)+1
 	for x in range(halfHours):
-		compiledData.append([0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0])
+		compiledData.append([0,0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0])
 		valueCount = 0
 		for y in castData:
 			if (y[0]//7) == x:
+				if valueCount == 0:
+					compiledData[x][1] = y[1]
 				valueCount += 1.0
-				compiledData[x][1] += y[1]
 				compiledData[x][2] += y[2]
 				compiledData[x][3] += y[3]
 				compiledData[x][4] += y[4]
-				compiledData[x][5] += y[5]
-				compiledData[x][6] += y[6]
-				compiledData[x][7] += y[7]
-				compiledData[x][8] += y[8]
+				if y[5] > 0 :
+					compiledData[x][5] += 1
+				if y[6] > 0 :
+					compiledData[x][6] += 1
+				if y[7] > 0 :
+					compiledData[x][7] += 1
+				if y[8] > 0 :
+					compiledData[x][8] += 1
+				compiledData[x][9] += y[9]
 		if valueCount > 0:
 			compiledData[x][0] = x
-			compiledData[x][1] = compiledData[x][1]/valueCount
 			compiledData[x][2] = compiledData[x][2]/valueCount
 			compiledData[x][3] = compiledData[x][3]/valueCount
 			compiledData[x][4] = compiledData[x][4]/valueCount
@@ -53,6 +60,7 @@ def compileHalfHour(keys,features,castData):
 			compiledData[x][6] = compiledData[x][6]/valueCount
 			compiledData[x][7] = compiledData[x][7]/valueCount
 			compiledData[x][8] = compiledData[x][8]/valueCount
+			compiledData[x][9] = compiledData[x][9]/valueCount
 		else:
 			compiledData[x][0] = -1
 		compiledData[x].insert(-1,valueCount)
@@ -65,10 +73,20 @@ def compileHalfHour(keys,features,castData):
 		X.append(entry[0:-1])
 	return X,Y
 	
+def timeParse(timestring):
+	time = timestring[11:-1]
+	hour,min,sec = time.split(":")
+	houri = int(hour)
+	mini = int(min)
+	seci = int(sec)
+	mini = mini*60
+	houri = houri*60*60
+	seconds = seci + mini + houri
+	return seconds
+
 okeys,ofeatures,ocastData = loadCSV("data/Subject_1.csv","data/list_1.csv")
 Xres,Yres = compileHalfHour(okeys,ofeatures,ocastData)
 X = np.array(Xres)
 Y = np.array(Yres)
 print X.shape
-print Y.shape
-print X
+print Y.shape		
